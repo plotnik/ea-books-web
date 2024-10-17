adf643c9f6130e4c924d7a03d7be000bad3f2d7e
Egor Abramovich <egor.abramovich@gmail.com>
2024-10-17 09:06:39 +0300

    books

--- public/lit/exc_ui.py
+++ public/lit/exc_ui.py
@@ -18,24 +18,33 @@ import subprocess
     
 log_files = [f for f in os.listdir('.') if f.endswith('.log')]
 
-# Step 2: Sort the log files alphabetically
+# Step 2: Sort log files based on their modification time
 #
 # ::
     
-log_files_sorted = sorted(log_files)
+log_files.sort(key=os.path.getmtime, reverse=True)
 
 # Step 3: Create radio buttons to select a file
 #
 # ::
     
-selected_file = st.radio("Select a log file to process:", log_files_sorted)
+selected_file = st.radio("Select a log file to process:", log_files)
 
 # Step 4: Button to process the selected file
 #
 # ::
     
-if st.button("Process"):
+if st.button(":rocket: Process"):
     subprocess.run(["exc", selected_file, '--html', '--skip=20', '--tformat=yyyy/MM/dd HH:mm:ss', '--restart=wrapper.tanukisoftware.org'], check=True)
     subprocess.run(["exc_yaml", selected_file], check=True)
   
     st.write(f"Processed: `{selected_file}`")
+
+# We can open log file in jEdit_ editor
+#
+# .. _jEdit: https://www.jedit.org
+#
+# ::
+
+if st.button(":pencil: jEdit"): 
+    subprocess.run(["/Applications/jEdit.app/Contents/MacOS/jedit", "-reuseview", os.path.abspath(selected_file)], check=True)
